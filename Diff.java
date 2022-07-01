a84ed5f222250512120e643ba273d5e781f08f3e
Abhishek Dubey <abhishekdubey95@gmail.com>
2022-07-01 19:11:08 +0530

    Changes to Diff.java

--- Diff.java
+++ Diff.java
@@ -1,6 +1,11 @@
 public class Diff {
  public static void main(String[] args) {
      
-        
+        int a = 267;
+        int b = 879;
+
+        int diff = b-a;
+
+        System.out.println(diff);
     }
 }
